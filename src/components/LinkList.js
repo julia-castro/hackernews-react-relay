e9fb78cec3c665f3b7d6a8317783fd5138eb9974
Julia Castro <castro.morales.julia@gmail.com>
2017-09-06 14:22:12 -0400

    linklist component, and mock data

---
+++ src/components/LinkList.js
@@ -0,0 +1,26 @@
+import react, { Component } from 'react'
+import Link from './Link'
+
+class LinkList extends Component {
+  render() {
+    const linksToRender = [{
+      id: '1',
+      description: 'The coolest GraphQL backend',
+      url: 'https://www.graph.cool'
+    }, {
+      id: '2',
+      description: 'Highly performant GraphQL client from Facebook',
+      url: 'https://facebook.github.io/relay/'
+    }]
+
+    return (
+      <div>
+        {linksToRender.map(link => (
+          <Link key={link.id} link={link}
+        ))}
+      </div>
+    )
+  }
+}
+
+export default LinkList
