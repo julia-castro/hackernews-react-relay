bf1c8ae09dd8412ca90d4a455b8d4fc8c1cc1d0d
Julia Castro <castro.morales.julia@gmail.com>
2017-09-06 14:56:14 -0400

    render returns linklistpage

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import LinkList from './LinkList'
+import LinkListPage from './LinkListPage'
 
 class App extends Component {
   render() {
     return (
-      <LinkList />
+      <LinkListPage />
     )
   }
 }
